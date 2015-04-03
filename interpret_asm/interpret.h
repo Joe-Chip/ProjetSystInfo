9352fef5b9b55f1f67497ca95dc78d75e807253b
Kevin Salaun <salaun.trk@gmail.com>
2015-04-03 13:55:04 +0200

    Legere avancee interpreteur

---
+++ interpret_asm/interpret.h
@@ -0,0 +1,13 @@
+#ifndef INTERPRET_H
+#define INTERPRET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAILLE_TABLE_VARS 256
+
+// Declaration de la table utilisee pour stocker les variables
+int table_variables[TAILLE_TABLE_VARS];
+
+
+#endif
